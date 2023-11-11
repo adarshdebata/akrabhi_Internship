3c21e7ebb423e196f416da216fcd8be0f26fe7b2
adarshdebata <adarshdebata00@gmail.com>
2023-11-11 20:36:43 +0530

    Day_5 Updated

---
+++ Day_5/revision.js
@@ -0,0 +1,71 @@
+const name1 = 'Peter';
+const name2 = "Jack";
+const result = `The names are ${name1} and ${name1}`;
+
+const message1 = 'This is a long message ' + 'that spans across multiple lines' + 'in the code.';
+console.log(`${message1}`);
+
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     JavaScript';
+
+
+const result1 = text1.concat(' ', text2);
+console.log(result1);
+
+const result2 = text1.toUpperCase();
+console.log(result2);
+
+
+const result3 = text3.trim();
+console.log(result3);
+
+
+const result5 = text1.slice(1, 3);
+console.log(result5);
+
+
+const name3 = 'Jack';
+console.log(`Hello ${name3}`);
+const results1 = 4 + 5;
+console.log(`The sum of 4 + 5 is ${result}`);
+console.log(`${result < 10 ? 'Too low' : 'Very high'}`);
+
+function tagExample(strings) {
+    return strings;
+}
+const results3 = tagExample`Hello Jack`;
+
+console.log(results3);
+
+let arr = [42, 23, 31];
+let a = arr.map((value) => {
+    console / log(value);
+    return value + 1;
+});
+console.log(a);
+
+function triple(n) {
+    return n * 3;
+}
+arr = new Array(1, 2, 3, 6, 5, 4);
+
+var new_arr = arr.map(triple)
+console.log(new_arr);
+
+function product(a, b) {
+    return a * b;
+}
+arr = new Array(1, 2, 3, 6, 5, 4);
+
+var product_of_arr = arr.reduce(product)
+console.log(product_of_arr)
+
+arr = new Array(1, 2, 3, 6, 5, 4);
+var new_arr = arr.filter(function (x) {
+    return x % 2 == 0;
+});
+
+console.log(new_arr)
+
+
